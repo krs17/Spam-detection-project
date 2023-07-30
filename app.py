f83983bcf512068f28fd036a6d5f435878a40984
KARTIK RAJ SHRIVASTAVA <kartikshrivastava5@gmail.com>
2023-07-30 20:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import string
-#from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 # from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
